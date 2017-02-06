5e66fd107da5839428df64300d3dd466a5b04294
“user1” <tyaa@ukr.net>
2017-02-06 18:44:17 +0300

    sort type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,6 +18,52 @@ namespace ConsoleApp1
                 count++;
                 Console.WriteLine("[{0}] {1}", count, item);
             }
+            bool flag = true;
+            int boof = 0;
+            //Сортировка пузырьками
+            do {
+                flag = false;
+                for (int i = 0; i < masInt.Length - 1; i++)
+                {
+                    if (masInt[i] > masInt[i + 1])
+                    {
+                        boof = masInt[i];
+                        masInt[i] = masInt[i + 1];
+                        masInt[i + 1] = boof;
+                        flag = true;
+                    }
+                }
+            } while (flag);
+            Console.WriteLine("Сортировка пузырьками");
+            count = 0;
+            foreach (int item in masInt)
+            {
+                count++;
+                Console.WriteLine("[{0}] {1}", count, item);
+            }
+            //Сортировка вставкой
+            int max;
+            for (int i = 0; i < masInt.Length; i++)
+            {
+                max = i;
+                for (int j = i+1; j < masInt.Length; j++)
+                {
+                    if (masInt[max] < masInt[j])
+                    {
+                        max = j;
+                    }
+                }
+                boof = masInt[i];
+                masInt[i] = masInt[max];
+                masInt[max] = boof;
+            }
+            Console.WriteLine("Сортировка вставкой");
+            count = 0;
+            foreach (int item in masInt)
+            {
+                count++;
+                Console.WriteLine("[{0}] {1}", count, item);
+            }
         }
     }
 }
